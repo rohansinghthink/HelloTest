3273af1fe9586a3cbc94f67ed05a62d4075e11b2
rohansinghthink <128616519+rohansinghthink@users.noreply.github.com>
2023-06-01 19:34:31 +0530

    Update test.py

--- test.py
+++ test.py
@@ -50,3 +50,31 @@ AWS_ACCESS_KEY_ID="AKASDSDKSDDKJ54LMFSL"
 AWS_SECRET_ACCESS_KEY="zcsdfdsnfkfshfdsjksdsdsdbjdnqm54lD5mTLf"
 
 
+
+Social Security Numbers (SSN):
+
+
+Example: 123-45-6789
+
+Credit Card Numbers (CCN):
+
+Example: 1234-5678-9012-3456
+
+Email Addresses:
+
+Example: example@example.com
+
+Passwords:
+
+Example: MySecurePassword123!
+
+API Keys:
+
+Example: a3e6b9fc279ebbbe1490f8b6d19f21a5
+
+Encryption Keys:
+
+Example: 2Lkj53gI9tR6XcP7
+
+
+
